ac42219f24b32f9b7dc30c1a006bd2d75f542546
AlexandrKov <mulah1988@mail.ru>
2023-10-26 09:19:54 +0300

    fixed test

--- src/__test__/test.js
+++ src/__test__/test.js
@@ -1,10 +1,10 @@
 import ErrorRepository from "../js/ErrorRepository"
 
-test("unknow error", () => {
+test("Неизвестная ошибка", () => {
     const errorRepo = new ErrorRepository;
-    expect(errorRepo.translate(407)).toThrow('Unknown error')
+    expect(errorRepo.translate(407)).toBe('Unknown error')
 })
-test("unknow error", () => {
+test("Получить текст ошибки", () => {
     const errorRepo = new ErrorRepository;
     errorRepo.addError(404, "Not Found")
     expect(errorRepo.translate(404)).toEqual("Not Found")
